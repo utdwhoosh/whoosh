9627672f5b0df6cd560e43b470670bc88008e510
Dust1n <d.grann@hotmail.com>
2015-09-16 10:48:35 -0500

    Added enums folder

---
+++ app/src/main/java/com/utdreqeng/whoosh/whoosh/Enums/EGroup.java
@@ -0,0 +1,8 @@
+package com.utdreqeng.whoosh.whoosh.Enums;
+
+/**
+ * Created by Dustin on 9/16/2015.
+ */
+public enum EGroup {
+    ATC, EXTERNAL, SSB
+}
---
+++ app/src/main/java/com/utdreqeng/whoosh/whoosh/Enums/EType.java
@@ -0,0 +1,8 @@
+package com.utdreqeng.whoosh.whoosh.Enums;
+
+/**
+ * Created by Dustin on 9/16/2015.
+ */
+public enum EType {
+    PATH, RAMP, SKYBRIDGE, ELEVATOR, DOOR, DOOR_IN, DOOR_OUT, ROOM, RESTROOM, STAIRS
+}
