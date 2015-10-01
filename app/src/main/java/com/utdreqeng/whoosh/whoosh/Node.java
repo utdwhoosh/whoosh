d4dbe004d70f01630865868113257fcfe85b7e4b
Frances Marie Imperial <imperialsoup@gmail.com>
2015-09-30 18:29:33 -0700

    parse integration

---
+++ app/src/main/java/com/utdreqeng/whoosh/whoosh/Node.java
@@ -0,0 +1,7 @@
+package com.utdreqeng.whoosh.whoosh;
+
+/**
+ * Created by Marie on 9/30/2015.
+ */
+public class Node {
+}
---
+++ app/src/main/java/com/utdreqeng/whoosh/whoosh/whooshApplication.java
@@ -0,0 +1,51 @@
+package com.utdreqeng.whoosh.whoosh;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class whooshApplication extends AppCompatActivity  {
+
+    //Parse App ID and client key
+    public static final String PARSE_APPLICATION_ID = "YB7DqZB3O0LfmIKzd9NzMQ6uhIXRhoawgK7p5B1M";
+    public static final String PARSE_CLIENT_KEY = "kA1LGDmyML7rhXEDkQjgNIKw8cNM5VoDpsO7Drxl";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable Local Datastore
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
+
+        //Test Parse connection
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
